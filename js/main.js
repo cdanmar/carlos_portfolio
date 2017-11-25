844a3df96670e58801f0953db015ce08424d0e97
cdanmar <cdanmar@gmail.com>
2017-11-24 19:05:38 -0600

    added some dynamic content to the portfolio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
-
-console.log("Webpack works");
+const myWork = [
+  {
+    'title': "Serverless Portfolio",
+    'image': {
+      'desc': "Lamnda Symbol",
+      'src': "images/AWS-Lambda.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Back of the Yards",
+    'image': {
+      'desc': "Coffee Beans",
+      'src': "images/volcancoffee1.jpg",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Event MGR",
+    'image': {
+      'desc': "placeholder image",
+      'src': "images/example3.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Lindsey Portfolio",
+    'image': {
+      'desc': "placeholder image",
+      'src': "images/example3.png",
+      'comment': ""
+    }
+  }
+]
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
